a6fa57f0d631a1cada79fcd74ed09fd5e17c1b8a
Gilmar Correia <gilmarjeronimo@uol.com.br>
2019-02-21 16:25:46 -0300

    Update Bioloid.java

--- AX12-JavA/ax12_JavA/src/Bioloid.java
+++ AX12-JavA/ax12_JavA/src/Bioloid.java
@@ -1,27 +1,26 @@
 
 public class Bioloid extends Ax12{
 		
-	public static int MAX_MOTOR;
-    public static int motors[] = new int[MAX_MOTOR];
-    public static int initPos[] = {336, 687, 298, 724, 412, 611, 355, 664, 491, 530, 394, 625, 278, 743, 616, 405, 490, 530};
+	private static int MAX_MOTOR;
+    private static int motors[] = new int[MAX_MOTOR];
+    private static int initPos[] = {336, 687, 298, 724, 412, 611, 355, 664, 491, 530, 394, 625, 278, 743, 616, 405, 490, 530};
     
 
-	Bioloid(int MAX_MOTORS){
+	public Bioloid(int MAX_MOTORS){
     	MAX_MOTOR = MAX_MOTORS; // NUMBER OF USED MOTORS 
-	}
-    		
+	}	
 
-    public static void initialPos() throws InterruptedException {
+    public void initialPos() throws InterruptedException {
         //serial();
         
         for (int i=0;i<motors.length;i++){
         	motors[i]=initPos[i];
-        	move(i+1,motors[i]);
+        	this.move(i+1,motors[i]);
         	Thread.sleep(1000);
         }
     }
     
-    public static void clear() throws InterruptedException {
+    public void clear() throws InterruptedException {
         //serial();
         
         for(int i=0;i<(motors.length);i++){
@@ -30,10 +29,11 @@ public class Bioloid extends Ax12{
         			motors[i] = 361;
         		if(i==(8-1))
         			motors[i] = 663;
-        	move(i+1,motors[i]);
+        	this.move(i+1,motors[i]);
         	Thread.sleep(1000);
         }
     }
+    
     
     //public static readMotors(){
     	
